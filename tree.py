9a693a2fb67ab7c22a019a538a75253de8c8eba6
Name Surname <93937463+reproductionprohibited@users.noreply.github.com>
2022-12-07 15:10:35 +0300

    Create tree.py

---
+++ tree.py
@@ -0,0 +1,30 @@
+def fir_tree(n):
+    if not (type(n) is int or type(n) is str):
+        raise TypeError(f"{n} is not an int")
+    else:
+        try:
+            n = int(n)
+            if n < 3:
+                return None
+
+            tree = ''
+            star_count, space_count = 1, n - 2
+            for _ in range(n - 1):
+                tree += (space_count * ' ' + star_count * '*') + '\n'
+                space_count -= 1
+                star_count += 2
+            tree += (n - 2) * ' ' + '|'
+            return tree
+        except ValueError as e:  # Если строка не является числом ("12", "185")
+            print("Illegal Parameter")
+            return None
+
+
+def main():
+    n = "12"
+    result = fir_tree(n)
+    print(result)
+
+
+if __name__ == "__main__":
+    main()
