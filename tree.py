403d96323c0205b3e7f7406df2922cc36cf61a88
Name Surname <93937463+reproductionprohibited@users.noreply.github.com>
2022-12-12 21:17:33 +0300

    Update tree.py

--- tree.py
+++ tree.py
@@ -1,23 +1,22 @@
 def tree(n):
     if not (type(n) is int or type(n) is str):
         raise TypeError(f"{n} is not a number")
-    else:
-        try:
-            n = int(n)
-            if n < 3:
-                return None
-
-            tree = ''
-            star_count, space_count = 1, n - 2
-            for _ in range(n - 1):
-                tree += (space_count * ' ' + star_count * '*') + '\n'
-                space_count -= 1
-                star_count += 2
-            tree += (n - 2) * ' ' + '|'
-            return tree
-        except ValueError as e:  # Если строка не является числом ("12", "185")
-            print("Illegal Parameter")
+    try:
+        n = int(n)
+        if n < 3:
             return None
+
+        tree = ''
+        star_count, space_count = 1, n - 2
+        for _ in range(n - 1):
+            tree += (space_count * ' ' + star_count * '*') + '\n'
+            space_count -= 1
+            star_count += 2
+        tree += (n - 2) * ' ' + '|'
+        return tree
+    except ValueError as e:  # Если строка не является числом ("12", "185")
+        print("Illegal Parameter")
+        return None
 
 
 def main():
